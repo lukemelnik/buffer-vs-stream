38bd184496ccf6c158e3914e49d2473ef9bd5104
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-04 16:00:36 -0400

    switch to objectMode

--- readable.js
+++ readable.js
@@ -15,13 +15,16 @@ const famous_quotes = [
 
 class StreamFromArray extends Readable {
   constructor(array) {
-    super({ encoding: "UTF-8" });
+    super({ objectMode: true });
     this.array = array;
     this.index = 0;
   }
   _read() {
     if (this.index <= this.array.length) {
-      const chunk = this.array[this.index];
+      const chunk = {
+        data: this.array[this.index],
+        index: this.index,
+      };
       this.push(chunk);
       this.index += 1;
     } else {
